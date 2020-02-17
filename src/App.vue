de35622cc0901e99229a0e7dd0ca502aa59a6244
Daemon-Macklin <daemon.macklin@outlook.com>
2020-02-17 15:36:18 +0000

    Starting on user page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <div>
+      <b-navbar toggleable="lg" type="light" variant="success">
+        <b-navbar-brand href="#">
+          <img src="./assets/logoNav.png" alt="Img">
+          MyT
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Link</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item to="/user">Profile</b-dropdown-item>
+              <b-dropdown-item href="/">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+  </div>
     <router-view/>
   </div>
 </template>
---
+++ src/components/User.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="section">
+        <div v-if="signedIn === false" class="ui raised centered segment">
+            Sign in form goes here
+        </div>
+        <div v-if="signedIn === true" class="ui raised centered segment">
+                User data goes here
+        </div>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import Cookies from 'vue-cookies'
+
+    Vue.use(Cookies)
+
+
+    export default {
+        name: "User",
+        data () {
+            return {
+                signedIn: false,
+                userName: null,
+                email: null,
+                id: null
+            }
+        }
+
+
+    }
+</script>
+
+<style scoped>
+    #section {
+        margin-left: 10%;
+        margin-right: 10%;
+    }
+</style>
